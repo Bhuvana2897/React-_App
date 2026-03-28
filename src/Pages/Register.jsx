e1989696104e4d46aa1262991e13d748c720d18f
Bhuvana2897 <bhuvaneshsampath97@gmail.com>
2026-03-28 13:28:09 +0530

    register to login page navigate

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,26 +1,54 @@
-import { Link } from "react-router"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router"
 
 
 const Register = () => {
+
+  const[ Name, setName] = useState("");
+  const [email , setEmail] = useState("");
+  const [password , setPassword] = useState("");
+
+  const Navigate = useNavigate();
+
+  const handlRegister =(e) => {
+      event.preventDefault() ;
+      console.log('Registering User ......')
+      console.log(`Name : ${Name}`)
+      console.log(`email : ${email}`)
+      console.log(`password : ${password}`)
+   
+      setTimeout(() =>{
+         alert('Registration Succesfull')
+         Navigate ("/Login") ; 
+      } , 500)
+
+  }
+
   return ( 
     <div>
          <h1 className="text-4xl p-4 ml-4"> Register </h1>
          <div className="w-xs mx-8 border p-4 border-gray-400 shadow-2xl">
-          <form className="flex flex-col gap-3">
+          <form className="flex flex-col gap-3" onSubmit = {handlRegister}>
              <input 
                   type="text"
                   className="border border w-full p-1"
                   placeholder="Your name......."
+                  value = {Name}
+                  onChange = {(e) => setName(e.target.value)}
               />
                 <input 
                   type="text"
                   className="border border w-full p-1"
                   placeholder="Your email......."
+                  value = {email}
+                  onChange = {(e) => setEmail(e.target.value)}
               />
               <input 
                   type="password"
                   className="border border w-full p-1"
                   placeholder="Your password......."
+                  value = {password}
+                  onChange = {(e) => setPassword(e.target.value)}
               />
               <button
                    type="submit"
